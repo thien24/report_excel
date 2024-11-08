0320489a716b12fc036b4092e338b7bd66191e89
vath.tr-dev <vthien562004@gmail.com>
2024-11-08 22:25:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const reportRoute = require('./Routes/reportRoute'); // Đường dẫn đến file reportRoute.js
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // Cấu hình CORS (nếu cần thiết)
 app.use(cors());
